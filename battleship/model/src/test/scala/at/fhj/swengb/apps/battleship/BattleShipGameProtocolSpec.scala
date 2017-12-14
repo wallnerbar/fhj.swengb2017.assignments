59e8653f6419fd8242c48ef220b4386260410a3b
Robert Ladstätter <rladstaetter@gmail.com>
2017-12-14 08:27:16 +0100

    bugfix for BattleshipGameGen

--- battleship/model/src/test/scala/at/fhj/swengb/apps/battleship/BattleShipGameProtocolSpec.scala
+++ battleship/model/src/test/scala/at/fhj/swengb/apps/battleship/BattleShipGameProtocolSpec.scala
@@ -6,14 +6,11 @@ import org.scalatest.WordSpecLike
 import org.scalatest.prop.Checkers
 
 
-class BattleShipGameProtocolSpec extends WordSpecLike {
+class BattleShipProtocolSpec extends WordSpecLike {
 
+  import at.fhj.swengb.apps.battleship.model.BattleshipGameGen._
 
-  val battleShipGameGen: Gen[BattleShipGame] = for {
-    battleField <- battleFieldGen
-  } yield BattleShipGame(battleField, i => i.toDouble, i => i.toDouble, println)
-
-  "BattleShipGameProtocol" should {
+  "BattleShipProtocol" should {
     "be deserializable" in {
       Checkers.check(Prop.forAll(battleShipGameGen) {
         expected: BattleShipGame => {
---
+++ battleship/model/src/test/scala/at/fhj/swengb/apps/battleship/model/BattleshipGameGen.scala
@@ -0,0 +1,13 @@
+package at.fhj.swengb.apps.battleship.model
+
+import org.scalacheck.Gen
+
+
+/**
+  * Implement in the same manner like MazeGen from the lab, adapt it to requirements of BattleShip
+  */
+object BattleshipGameGen {
+
+  val battleShipGameGen: Gen[BattleShipGame] = ???
+
+}
